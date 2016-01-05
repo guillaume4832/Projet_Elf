2db0f84fcca43563d5edd6e6f035bed787429a4a
lucassA <lucas.albarede4b@laposte.net>
2016-01-05 13:14:32 +0100

    Create etape1.h

---
+++ etape1.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <assert.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <arpa/inet.h>
+
+#include <elf.h>
+
+bool elf_check_file(Elf32_Ehdr *hdr);
+
+bool elf_check_supported(Elf32_Ehdr *hdr);
+
+void print_elf_header(Elf32_Ehdr *elf_header);
+
+void *elf_load_file(void *file);
