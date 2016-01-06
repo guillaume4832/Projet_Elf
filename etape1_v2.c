7fabd71f366d9ab531f314e49cdfad98399f455d
Amédée Fleau <afleau2@hotmail.com>
2016-01-06 13:33:15 +0100

    Version finale de l'étape 1   - Ajout de toutes les infos du header   - Mode verbose paramétrable

--- etape1_v2.c
+++ etape1_v2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <elf.h>
 
 char* readFileBytes(const char *name)
 {
@@ -13,59 +14,78 @@ char* readFileBytes(const char *name)
     return ret;
 }
 
-int main(int argc, char * argv[]){
+Elf32_Ehdr readHeader(char * filePath, int isVerbose){
+    Elf32_Ehdr header;
     int isValid = 1;
     int is64 = 0;
-    unsigned char* fileBytes = readFileBytes(argv[1]);
+    unsigned char* fileBytes = readFileBytes(filePath);
     /* Lecture des bits 'magiques' ELF */
     if(fileBytes[0] == 127 && fileBytes[1] == 69 && fileBytes[2] == 76 && fileBytes[3] == 70){
-        printf("[*] Fichier ELF Reconnu\n");
+        if(isVerbose == 1)
+            printf("[*] Fichier ELF Reconnu\n");
+        header.e_ident[EI_MAG0] = fileBytes[0];
+        header.e_ident[EI_MAG1] = fileBytes[1];
+        header.e_ident[EI_MAG2] = fileBytes[2];
+        header.e_ident[EI_MAG3] = fileBytes[3];
     }
     else{
-        printf("[E] Le fichier founi n'est pas un fichier ELF\n");
+        if(isVerbose == 1)
+            printf("[E] Le fichier founi n'est pas un fichier ELF\n");
         isValid = 0;
     }
     int i = 4;
     /* Lecture de la classe */
     if(isValid == 1){
         if(fileBytes[i] == 1){
-            printf("[*] Classe: 32 bits ELFCLASS32\n");
+            if(isVerbose == 1)
+                printf("[*] Classe: 32 bits ELFCLASS32\n");
+
         }
         else if(fileBytes[i] == 2){
-            printf("[*] Classe: 64 bits ELFCLASS64\n");
+            if(isVerbose == 1)
+                printf("[*] Classe: 64 bits ELFCLASS64\n");
             is64 = 1;
         }
         else{
-            printf("[E] Erreur de lecture de la classe du fichier ELF\n");
+            if(isVerbose == 1)
+                printf("[E] Erreur de lecture de la classe du fichier ELF\n");
             isValid = 0;
         }
+        header.e_ident[EI_CLASS] = fileBytes[i];
     }
     i++;
 
     /* Lecture de l'encodage des données processeur */
     if(isValid == 1){
         if(fileBytes[i] == 1){
-            printf("[*] EI_DATA: ELFDATA2LSB Little endian\n");
+            if(isVerbose == 1)
+                printf("[*] EI_DATA: ELFDATA2LSB Little endian\n");
         }
         else if(fileBytes[i] == 2){
-            printf("[*] EI_DATA : ELFDATA2MSB Big endian\n");
+            if(isVerbose == 1)
+                printf("[*] EI_DATA : ELFDATA2MSB Big endian\n");
         }
         else{
-            printf("[E] Erreur de lecture de l'encodage EI_DATA ELF\n");
+            if(isVerbose == 1)
+                printf("[E] Erreur de lecture de l'encodage EI_DATA ELF\n");
             isValid = 0;
         }
+        header.e_ident[EI_DATA] = fileBytes[i];
     }
     i++;
 
     /* Lecture de la version du header */
     if(isValid == 1){
         if(fileBytes[i] != 0){
-            printf("[*] Version du header : %d\n",fileBytes[i]);
+            if(isVerbose == 1)
+                printf("[*] Version du header : %d\n",fileBytes[i]);
         }
         else{
-            printf("[E] Version du header invalide\n");
+            if(isVerbose == 1)
+                printf("[E] Version du header invalide\n");
             isValid = 0;
         }
+        header.e_ident[EI_VERSION] = fileBytes[i];
     }
     i++;
 
@@ -73,59 +93,77 @@ int main(int argc, char * argv[]){
 
     if(isValid == 1){
         if(fileBytes[i] == 0){
-            printf("[*] OS/ABI : Unix System V\n");
+            if(isVerbose == 1)
+                printf("[*] OS/ABI : Unix System V\n");
         }
         else if(fileBytes[i] == 1){
-            printf("[*] OS/ABI : HP-UX\n");
+            if(isVerbose == 1)
+                printf("[*] OS/ABI : HP-UX\n");
         }
         else if(fileBytes[i] == 2){
-            printf("[*] OS/ABI : NetBSD\n");
+            if(isVerbose == 1)
+                printf("[*] OS/ABI : NetBSD\n");
         }
         else if(fileBytes[i] == 3){
-            printf("[*] OS/ABI : Linux\n");
+            if(isVerbose == 1)
+                printf("[*] OS/ABI : Linux\n");
         }
         else if(fileBytes[i] == 6){
-            printf("[*] OS/ABI : Sun Solaris\n");
+            if(isVerbose == 1)
+                printf("[*] OS/ABI : Sun Solaris\n");
         }
         else if(fileBytes[i] == 7){
-            printf("[*] OS/ABI : IBM AIX\n");
+            if(isVerbose == 1)
+                printf("[*] OS/ABI : IBM AIX\n");
         }
         else if(fileBytes[i] == 8){
-            printf("[*] OS/ABI : SGI Irix\n");
+            if(isVerbose == 1)
+                printf("[*] OS/ABI : SGI Irix\n");
         }
         else if(fileBytes[i] == 9){
-            printf("[*] OS/ABI : FreeBSD\n");
+            if(isVerbose == 1)
+                printf("[*] OS/ABI : FreeBSD\n");
         }
         else if(fileBytes[i] == 10){
-            printf("[*] OS/ABI : Compaq TRU64\n");
+            if(isVerbose == 1)
+                printf("[*] OS/ABI : Compaq TRU64\n");
         }
         else if(fileBytes[i] == 11){
-            printf("[*] OS/ABI : Novell Modesto\n");
+            if(isVerbose == 1)
+                printf("[*] OS/ABI : Novell Modesto\n");
         }
         else if(fileBytes[i] == 12){
-            printf("[*] OS/ABI : OpenBSD\n");
+            if(isVerbose == 1)
+                printf("[*] OS/ABI : OpenBSD\n");
         }
         else if(fileBytes[i] == 64){
-            printf("[*] OS/ABI : ARL EABI\n");
+            if(isVerbose == 1)
+                printf("[*] OS/ABI : ARL EABI\n");
         }
         else if(fileBytes[i] == 97){
-            printf("[*] OS/ABI : ARM\n");
+            if(isVerbose == 1)
+                printf("[*] OS/ABI : ARM\n");
         }
         else if(fileBytes[i] == 255){
-            printf("[*] OS/ABI : Standalone\n");
+            if(isVerbose == 1)
+                printf("[*] OS/ABI : Standalone\n");
         }
         else{
-            printf("[W] OS/ABI : Inconnu\n");
-            printf("%d\n",fileBytes[i]);
+            if(isVerbose == 1){
+                printf("[W] OS/ABI : Inconnu\n");
+                printf("%d\n",fileBytes[i]);
+            }
             isValid = 0;
         }
-
+        header.e_ident[EI_OSABI] = fileBytes[i];
     }
     i++;
 
     /* Lecture de la version de l'ABI */
     if(isValid == 1){
-        printf("[*] Version de l'ABI : %d\n",fileBytes[i]);
+        if(isVerbose == 1)
+            printf("[*] Version de l'ABI : %d\n",fileBytes[i]);
+        header.e_ident[EI_ABIVERSION] = fileBytes[i];
     }
     i++;
 
@@ -133,7 +171,8 @@ int main(int argc, char * argv[]){
     if(isValid == 1){
         for(i;i<15;i++){
             if(fileBytes[i] != 0){
-                printf("[E] Bits de reserves EI_PAD non réservés à 0");
+                if(isVerbose == 1)
+                    printf("[E] Bits de reserves EI_PAD non réservés à 0");
                 isValid = 0;
                 break;
             }
@@ -143,80 +182,90 @@ int main(int argc, char * argv[]){
 
     /* Lecture du type de l'objet */
     if(isValid == 1){
-        if(fileBytes[i] == 0){
-            printf("[*] Pas de type de fichier\n");
+        if(isVerbose == 1){
+            if(fileBytes[i] == 0){
+                printf("[*] Pas de type de fichier\n");
+            }
+            else if(fileBytes[i] == 1){
+                printf("[*] Objet de type : Repositionable (Relocatable)\n");
+            }
+            else if(fileBytes[i] == 2){
+                printf("[*] Objet de type : Executable\n");
+            }
+            else if(fileBytes[i] == 3){
+                printf("[*] Objet de type : Partagé\n");
+            }
+            else if(fileBytes[i] == 4){
+                printf("[*] Objet de type : Core\n");
+            }
+            else{
+                printf("[W] Type inconnu\n");
+            }
         }
-        else if(fileBytes[i] == 1){
-            printf("[*] Objet de type : Repositionable (Relocatable)\n");
-        }
-        else if(fileBytes[i] == 2){
-            printf("[*] Objet de type : Executable\n");
-        }
-        else if(fileBytes[i] == 3){
-            printf("[*] Objet de type : Partagé\n");
-        }
-        else if(fileBytes[i] == 4){
-            printf("[*] Objet de type : Core\n");
-        }
-        else{
-            printf("[W] Type inconnu\n");
-        }
+        header.e_type = fileBytes[i];
     }
     i+=2;
 
     /* Lecture de la machine cible */
     if(isValid == 1){
-        if(fileBytes[i] == 0){
-            printf("[*] Aucune machine cible\n");
+        if(isVerbose == 1){
+            if(fileBytes[i] == 0){
+                printf("[*] Aucune machine cible\n");
+            }
+            else if(fileBytes[i] == 2){
+                printf("[*] Machine cible : SPARC\n");
+            }
+            else if(fileBytes[i] == 3){
+                printf("[*] Machine cible : Intel 80386\n");
+            }
+            else if(fileBytes[i] == 4){
+                printf("[*] Machine cible : Motorola 68000\n");
+            }
+            else if(fileBytes[i] == 7){
+                printf("[*] Machine cible : Intel i860\n");
+            }
+            else if(fileBytes[i] == 8){
+                printf("[*] Machine cible : MIPS I\n");
+            }
+            else if(fileBytes[i] == 19){
+                printf("[*] Machine cible : Intel i960\n");
+            }
+            else if(fileBytes[i] == 20){
+                printf("[*] Machine cible : PowerPC\n");
+            }
+            else if(fileBytes[i] == 40){
+                printf("[*] Machine cible : ARM\n");
+            }
+            else if(fileBytes[i] == 50){
+                printf("[*] Machine cible : Intel IA64\n");
+            }
+            else if(fileBytes[i] == 62){
+                printf("[*] Machine cible : x64\n");
+            }
+            else{
+                printf("[W] Machine cible non reconnue\n");
+            }
         }
-        else if(fileBytes[i] == 2){
-            printf("[*] Machine cible : SPARC\n");
-        }
-        else if(fileBytes[i] == 3){
-            printf("[*] Machine cible : Intel 80386\n");
-        }
-        else if(fileBytes[i] == 4){
-            printf("[*] Machine cible : Motorola 68000\n");
-        }
-        else if(fileBytes[i] == 7){
-            printf("[*] Machine cible : Intel i860\n");
-        }
-        else if(fileBytes[i] == 8){
-            printf("[*] Machine cible : MIPS I\n");
-        }
-        else if(fileBytes[i] == 19){
-            printf("[*] Machine cible : Intel i960\n");
-        }
-        else if(fileBytes[i] == 20){
-            printf("[*] Machine cible : PowerPC\n");
-        }
-        else if(fileBytes[i] == 40){
-            printf("[*] Machine cible : ARM\n");
-        }
-        else if(fileBytes[i] == 50){
-            printf("[*] Machine cible : Intel IA64\n");
-        }
-        else if(fileBytes[i] == 62){
-            printf("[*] Machine cible : x64\n");
-        }
-        else{
-            printf("[W] Machine cible non reconnue\n");
-        }
+        header.e_machine = fileBytes[i];
     }
     i+=2;
 
     /* Lecture de la version du fichier */
     if(isValid == 1){
         if(fileBytes[i] == 0){
-            printf("[E] Version invalide\n");
+            if(isVerbose == 1)
+                printf("[E] Version invalide\n");
             isValid = 0;
         }
         else if(fileBytes[i] == 1){
-            printf("[*] Version 0x1\n");
+            if(isVerbose == 1)
+                printf("[*] Version 0x1\n");
         }
         else{
-            printf("[W] Version inconnue\n");
+            if(isVerbose == 1)
+                printf("[W] Version inconnue\n");
         }
+        header.e_version = fileBytes[i];
     }
     i+=4;
 
@@ -226,7 +275,9 @@ int main(int argc, char * argv[]){
         unsigned int third = fileBytes[i+2];
         unsigned int fourth = fileBytes[i+3];
         unsigned int sum = first + second *16*16 + third *16*16*16*16 + fourth *16*16*16*16*16*16;
-        printf("[*] Adresse du point d'entrée : 0x%x\n",sum);
+        if(isVerbose == 1)
+            printf("[*] Adresse du point d'entrée : 0x%x\n",sum);
+        header.e_entry = sum;
     }
     i+=4;
 
@@ -236,7 +287,9 @@ int main(int argc, char * argv[]){
         unsigned int third = fileBytes[i+2];
         unsigned int fourth = fileBytes[i+3];
         unsigned int sum = first + second *16*16 + third *16*16*16*16 + fourth *16*16*16*16*16*16;
-        printf("[*] Début des en-têtes de programme : %d\n",sum);
+        if(isVerbose == 1)
+            printf("[*] Début des en-têtes de programme : %d\n",sum);
+        header.e_phoff = sum;
     }
     i+=4;
 
@@ -246,7 +299,9 @@ int main(int argc, char * argv[]){
         unsigned int third = fileBytes[i+2];
         unsigned int fourth = fileBytes[i+3];
         unsigned int sum = first + second *16*16 + third *16*16*16*16 + fourth *16*16*16*16*16*16;
-        printf("[*] Début des en-têtes de section : %d\n",sum);
+        if(isVerbose == 1)
+            printf("[*] Début des en-têtes de section : %d\n",sum);
+        header.e_shoff = sum;
     }
     i+=4;
 
@@ -256,7 +311,9 @@ int main(int argc, char * argv[]){
         unsigned int third = fileBytes[i+2];
         unsigned int fourth = fileBytes[i+3];
         unsigned int sum = first + second *16*16 + third *16*16*16*16 + fourth *16*16*16*16*16*16;
-        printf("[*] Fanions : 0x%x\n",sum);
+        if(isVerbose == 1)
+            printf("[*] Fanions : 0x%x\n",sum);
+        header.e_flags = sum;
     }
     i+=4;
 
@@ -264,7 +321,9 @@ int main(int argc, char * argv[]){
         unsigned int first = fileBytes[i];
         unsigned int second = fileBytes[i+1];
         unsigned int sum = first + second *16*16;
-        printf("[*] Taille de cet en-tête : %d\n",sum);
+        if(isVerbose == 1)
+            printf("[*] Taille de cet en-tête : %d\n",sum);
+        header.e_ehsize = sum;
     }
     i+=2;
 
@@ -272,7 +331,9 @@ int main(int argc, char * argv[]){
         unsigned int first = fileBytes[i];
         unsigned int second = fileBytes[i+1];
         unsigned int sum = first + second *16*16;
-        printf("[*] Taille de l'en-tête du programme : %d\n",sum);
+        if(isVerbose == 1)
+            printf("[*] Taille de l'en-tête du programme : %d\n",sum);
+        header.e_phentsize = sum;
     }
     i+=2;
 
@@ -280,7 +341,9 @@ int main(int argc, char * argv[]){
         unsigned int first = fileBytes[i];
         unsigned int second = fileBytes[i+1];
         unsigned int sum = first + second *16*16;
-        printf("[*] Nombre d'en-tête du programme : %d\n",sum);
+        if(isVerbose == 1)
+            printf("[*] Nombre d'en-tête du programme : %d\n",sum);
+        header.e_phnum = sum;
     }
     i+=2;
 
@@ -288,7 +351,9 @@ int main(int argc, char * argv[]){
         unsigned int first = fileBytes[i];
         unsigned int second = fileBytes[i+1];
         unsigned int sum = first + second *16*16;
-        printf("[*] Taille des en-têtes de section : %d\n",sum);
+        if(isVerbose == 1)
+            printf("[*] Taille des en-têtes de section : %d\n",sum);
+        header.e_shentsize = sum;
     }
     i+=2;
 
@@ -296,7 +361,9 @@ int main(int argc, char * argv[]){
         unsigned int first = fileBytes[i];
         unsigned int second = fileBytes[i+1];
         unsigned int sum = first + second *16*16;
-        printf("[*] Nombre d'en-tête de section : %d\n",sum);
+        if(isVerbose == 1)
+            printf("[*] Nombre d'en-tête de section : %d\n",sum);
+        header.e_shnum = sum;
     }
     i+=2;
 
@@ -304,10 +371,17 @@ int main(int argc, char * argv[]){
         unsigned int first = fileBytes[i];
         unsigned int second = fileBytes[i+1];
         unsigned int sum = first + second *16*16;
-        printf("[*] Table d'indexes des chaînes d'en-tête de section : %d\n",sum);
+        if(isVerbose == 1)
+            printf("[*] Table d'indexes des chaînes d'en-tête de section : %d\n",sum);
+        header.e_shstrndx = sum;
     }
     i+=2;
 
 
+    return header;
+}
+
+int main(int argc, char * argv[]){
+    Elf32_Ehdr header = readHeader(argv[1],1);
     return 0;
 }
