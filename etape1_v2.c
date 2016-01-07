ce0b6608c219e068be97edc44763ad1c45395643
Amédée Fleau <afleau2@hotmail.com>
2016-01-07 10:44:22 +0100

    Section 3 terminée

--- etape1_v2.c
+++ etape1_v2.c
@@ -503,8 +503,8 @@ void readSheader(char * filePath, Elf32_Ehdr header,Elf32_Shdr* shdr,int isVerbo
 
 
 	}
-
-	printf("  [Nr] 			Nom\t\tType\t\tAdr\t\tDécala.\tTaille\tES\tFan\tLN\tInf\tAl\n");
+    if(isVerbose == 1)
+	   printf("  [Nr] 			Nom\t\tType\t\tAdr\t\tDécala.\tTaille\tES\tFan\tLN\tInf\tAl\n");
 
 	for(i=0;i<header.e_shnum;i++){
 
@@ -524,16 +524,89 @@ void readSheader(char * filePath, Elf32_Ehdr header,Elf32_Shdr* shdr,int isVerbo
 		}
         name[n] = 0;
 
-
-		printf("  [%d] %20s\t\t%d\t\t%08d\t%06x\t%x\t%x\t%d\t%d\t%d\t%d\n",i,name,shdr[i].sh_type,shdr[i].sh_addr,shdr[i].sh_offset,shdr[i].sh_size,shdr[i].sh_entsize,shdr[i].sh_flags,shdr[i].sh_link,shdr[i].sh_info,shdr[i].sh_addralign);
+        if(isVerbose == 1)
+		      printf("  [%2d] %20s\t\t%d\t\t%08d\t%06x\t%x\t%x\t%d\t%d\t%d\t%d\n",i,name,shdr[i].sh_type,shdr[i].sh_addr,shdr[i].sh_offset,shdr[i].sh_size,shdr[i].sh_entsize,shdr[i].sh_flags,shdr[i].sh_link,shdr[i].sh_info,shdr[i].sh_addralign);
 	}
+
+}
+
+void readSection(int numsection,char * filePath,Elf32_Ehdr header,Elf32_Shdr* shdr){
+    int i;
+    char* name = malloc(sizeof(char)*75);
+    unsigned char* fileBytes = readFileBytes(filePath); // Contenu du fichier
+    int addrStrTable = shdr[header.e_shstrndx].sh_offset; // Récupération de l'adresse de la string table
+    int n = 0;
+    int j = addrStrTable + shdr[numsection].sh_name;
+
+    while (fileBytes[j] != 0)
+    {
+        name[n] = fileBytes[j];
+        j++;
+        n++;
+
+    }
+    name[n] = 0;
+    printf("\nVidange hexadécimale de la section < %s > :\n",name);
+
+    int addr = shdr[numsection].sh_offset; // Adresse de la section
+    int size = shdr[numsection].sh_size; // Taille de la section
+    char * tampon = malloc(sizeof(char)*16);
+    int idxTab = 1;
+    int startLine = 1;
+    int adrHex = 0;
+
+    for(i=0;i<size;i++){
+        if(i%4 == 3)
+        {
+            printf("%02x ",fileBytes[addr+i]);
+            idxTab++;
+        }
+        else{
+            if(startLine == 1){
+                startLine = 0;
+                printf("  0x%08x ",adrHex);
+                adrHex+=16;
+            }
+            printf("%02x",fileBytes[addr+i]);
+        }
+
+        tampon[i%16] = fileBytes[addr+i];
+        if(tampon[i%16] < 33 || tampon[i%16] > 126){
+            tampon[i%16] = '.';
+        }
+        if(i%16 == 15){
+            printf("\t %s",tampon);
+            tampon[0] = 0;
+            printf("\n");
+            startLine = 1;
+            idxTab = 1;
+        }
+    }
+    tampon[i%16] = 0;
+    int k =0;
+    for(k;k<i%8;k++){
+        printf(" ");
+    }
+    if(size%8==0)
+        idxTab--;
+    for(k=0;k<4-idxTab;k++){
+
+        printf("         ");
+    }
+    printf("\t %s",tampon);
+    printf("\n\n");
 
 }
 
 
 int main(int argc, char * argv[]){
+    /* Lecture du header */
     Elf32_Ehdr header = readHeader(argv[1],0);
+    /* Lecture des headers de sections */
 	Elf32_Shdr shdr[header.e_shnum];
 	readSheader(argv[1],header,shdr,0);
+    /* Lecture d'une section (contenu) */
+    readSection(12,argv[1],header,shdr);
+
     return 0;
 }
