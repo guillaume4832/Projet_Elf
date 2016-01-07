521470a5ba0c9a5b35b9156ecacd22d1c2181deb
albaredl <albaredl@f215-05.e-im2ag.ujf-grenoble.fr>
2016-01-07 14:04:56 +0100

    Finalisation de l'étape 2

--- etape1_v2.c
+++ etape1_v2.c
@@ -14,6 +14,114 @@ char* readFileBytes(const char *name)
     return ret;
 }
 
+char* nom_type(Elf32_Word sh_type)
+{
+  switch (sh_type)
+    {
+    case SHT_NULL:		 	return "NULL";
+    case SHT_PROGBITS:		return "PROGBITS";
+    case SHT_SYMTAB:		return "SYMTAB";
+    case SHT_STRTAB:		return "STRTAB";
+    case SHT_RELA:			return "RELA";
+    case SHT_HASH:			return "HASH";
+    case SHT_DYNAMIC:		return "DYNAMIC";
+    case SHT_NOTE:			return "NOTE";
+    case SHT_NOBITS:		return "NOBITS";
+    case SHT_REL:			return "REL";
+    case SHT_SHLIB:			return "SHLIB";
+    case SHT_DYNSYM:		return "DYNSYM";
+    case SHT_INIT_ARRAY:	return "INIT_ARRAY";
+    case SHT_FINI_ARRAY:	return "FINI_ARRAY";
+    case SHT_PREINIT_ARRAY:	return "PREINIT_ARRAY";
+    case SHT_GROUP:			return "GROUP";
+    case SHT_SYMTAB_SHNDX:	return "SYMTAB SECTION INDICIES";
+    case SHT_GNU_verdef:	return "VERDEF";
+    case SHT_GNU_verneed:	return "VERNEED";
+    case SHT_GNU_versym:	return "VERSYM";
+    case 0x6ffffff0:		return "VERSYM";
+    case 0x6ffffffc:		return "VERDEF";
+    case 0x7ffffffd:		return "AUXILIARY";
+    case 0x7fffffff:		return "FILTER";
+    case SHT_GNU_LIBLIST:	return "GNU_LIBLIST";
+
+	default: return "";
+}
+}
+
+
+char* nom_flags(unsigned int flags) {
+	
+	char* val = malloc(sizeof(char)*9);
+	
+	int p;
+	for(p=0;p<9;p++) {
+			val[p]='\0';
+	}
+	int r;
+	int i = 1;
+	int b = 0;
+	while (flags != 0)
+	{
+		r = flags%2;
+        flags = flags/2;
+        b = b+r*i;
+        i = i*10;
+	}
+
+
+    
+	if (b & 00000001)
+	{
+		val[0]='W';
+		if (b & 00000010)
+		{
+		val[1]='A';
+			if (b & 00000100)
+			{
+			val[2]='X';
+			}
+		}
+		else if (b & 00000100)
+			{
+			val[1]='X';
+			}
+	}
+
+	else if (b & 00000010)
+	{
+		val[0]='A';
+		if (b & 00000100)
+		{
+		val[1]='X';
+		}
+	}
+
+	else if (b & 00000100) {
+		val[0]='X';
+		}
+
+	if(b!=0 && val[0]=='\0') {
+	val = "U";
+
+	}
+
+	return val;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 Elf32_Ehdr readHeader(char * filePath, int isVerbose){
     Elf32_Ehdr header;
     int isValid = 1;
@@ -504,13 +612,13 @@ void readSheader(char * filePath, Elf32_Ehdr header,Elf32_Shdr* shdr,int isVerbo
 		
 	}
 
-	printf("  [Nr] 			Nom\t\tType\t\tAdr\t\tDécala.\tTaille\tES\tFan\tLN\tInf\tAl\n");
+	printf("  [Nr]  Nom\t\t        Type\t    Adr    \tDécala.\tTaille\tES\tFan\tLN\tInf\tAl\n");
 
 	for(i=0;i<header.e_shnum;i++){
 
 
 		// addrStrTable
-		char* name;
+		char* name = malloc(sizeof(char)*75);
 		int n = 0;
 		int j = addrStrTable + shdr[i].sh_name;
 		
@@ -524,10 +632,11 @@ void readSheader(char * filePath, Elf32_Ehdr header,Elf32_Shdr* shdr,int isVerbo
 		}
 		name[n]=0;
 		
-		 
+	
 
-		printf("  [%d] %20s\t\t%d\t\t%08d\t%06x\t%x\t%x\t%d\t%d\t%d\t%d\n",i,name,shdr[i].sh_type,shdr[i].sh_addr,shdr[i].sh_offset,shdr[i].sh_size,shdr[i].sh_entsize,shdr[i].sh_flags,shdr[i].sh_link,shdr[i].sh_info,shdr[i].sh_addralign);		
+		printf("  [%2d] %-25s%-12s%08d\t%06x\t%06x\t%02x\t%-1.04s\t%d\t%d\t%d\n",i,name,nom_type(shdr[i].sh_type),shdr[i].sh_addr,shdr[i].sh_offset,shdr[i].sh_size,shdr[i].sh_entsize,nom_flags(shdr[i].sh_flags),shdr[i].sh_link,shdr[i].sh_info,shdr[i].sh_addralign);		
 	}
+	printf (("Clé des fanions:\n W (écriture), A (allocation), X (exécution), U (Valeur Inconnue)\n"));
 	
 }
 
